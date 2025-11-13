21b8ea35b52bda0572ba49fde11259ad8a545e27
Omar Almasri <sharparray1994@gmail.com>
2025-11-13 16:07:56 +0300

    implement Generate AI Trips with Gemini

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import {type RouteConfig, index, route, layout} from "@react-router/dev/routes";
 export default [index("routes/home.tsx"),
 
     route('sign-in', 'routes/root/sign-in.tsx'),
+    route('api/create-trip', 'routes/api/create-trip.ts'),
 
     layout('routes/admin/admin-layout.tsx', [
         route('dashboard', 'routes/admin/dashboard.tsx'),
